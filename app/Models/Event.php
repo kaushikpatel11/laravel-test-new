66a03f27fab5e138cfcce6daf18c43c395d83bc4
devunivisionz <dev.univisionz@gmail.com>
2022-06-25 17:17:53 +0530

    Events Listing

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -5,6 +5,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Event extends Model
-{
+class Event extends Model{
+
+    public function workshops(){
+        return $this->hasMany(Workshop::class,'event_id','id');
+    }
 }
